22b8a1dccbd7218965da0e2762dba63fdc967ddb
RizmyAbdulla <rizmyabdulla@gmail.com>
2022-12-02 23:04:15 +0530

    accordion added

--- script.js
+++ script.js
@@ -1,66 +1,61 @@
-function accordion1 (code) {
-
-	this.code = `
-<div class="accordion" id="accordionExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="headingOne">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Accordion Item #1
-      </button>
-    </h2>
-    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-    </div>
-  </div>
-  </div>
-</div>`
-}
-
 function accordion() {
+	type("accordion");
 	let html = document.getElementById("content").innerHTML = `
-<div class="accordion" id="accordionExample">
+<div class="accordion" id="accordion">
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingOne">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="flush-collapseOne">
         Accordion Item #1
       </button>
     </h2>
-    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordion">
       <div class="accordion-body">
         <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
     </div>
   </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="headingTwo">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Accordion Item #2
-      </button>
-    </h2>
-    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="headingThree">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Accordion Item #3
-      </button>
-    </h2>
-    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
   </div>
 </div>`;
-	document.getElementById("btn").className = "btn btn-primary";
-
-	console.log("Printed")
+//	document.getElementById("btn").className = "btn btn-primary";
 }
 function Alerts() {
+	type("Alert");
 	console.log("Printed")
+}
+
+function type(fname){
+	let tfa = ["accordion", "flush-","accordion-flush"];
+	if(fname == "accordion"){
+			let tcomps = `<button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    						Type
+  					</button>
+  					<ul class="dropdown-menu dropdown-menu-dark" id="tcontent">
+    					<li><a class="dropdown-item" onclick="def()">default</a></li>
+    		  			<li><a class="dropdown-item" onclick="flush()">Flush</a></li>
+    				</ul>
+
+    				<button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    						Always open
+  					</button>
+  					<ul class="dropdown-menu dropdown-menu-dark" id="tacontent">
+    					<li><a class="dropdown-item" onclick="def()">True</a></li>
+    		  			<li><a class="dropdown-item" onclick="flush()">False</a></li>
+    				</ul>
+
+    				`
+	document.getElementById('components').innerHTML += tcomps;
+
+	}else{
+		tcomps = ``;
+		console.log("unexpected")
+	}
+}
+function flush() {
+    	document.getElementById("accordion").classList.add("accordion-flush");
+    	//document.getElementById("accordion").classList.add(tfa[2]);
+    	console.log(document.getElementById("accordion").className);
+    	console.log("success");
+   }
+function def () {
+	document.getElementById("accordion").className = "accordion"
+	console.log("success")
 }
