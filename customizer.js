b3a7e63d898068e8d3e36a22e628d023a63b59f4
RizmyAbdulla <rizmyabdulla@gmail.com>
2022-12-02 21:22:24 +0530

    init

---
+++ customizer.js
@@ -0,0 +1,15 @@
+let ac = ["accordion","accordion accordion-flush"]
+let accordionclass = {
+	data_bs_target:[
+	"#panelsStayOpen-collapse1"
+	],
+
+	aria_labelledby:[
+	"panelsStayOpen-heading1"
+	]
+}
+let btn = ["btn btn-primary","btn btn-secondary"]
+let types = [ btn[0]]
+
+console.log(types)
+console.log(accordionclass.data_bs_target[0])
